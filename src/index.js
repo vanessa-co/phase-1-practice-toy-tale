788c2378e7aad7e3a39ae77bb26949f323b4cc43
Seyfican Yalcindag <+>
2023-02-07 19:08:22 +0300

    step one complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +14,43 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetch("http://localhost:3000/toys")
+    .then((res) => res.json() )
+    .then((json) => loadToys(json))
+    // With response data make a div with the class of card with each toy
+    // add each toy to the div toy-collection
+
+    function loadToys(json) {
+      json.forEach(makeToyCard);
+    }
+
+    function makeToyCard(toyObject) {
+
+        const toyCardDiv = document.getElementById("toy-collection")
+        let toyCard = document.createElement("div")
+        toyCard.className = "card"
+        
+
+        const toyH2 = document.createElement("h2")
+        toyH2.textContent = toyObject.name
+
+        const toyImg = document.createElement("img")
+        toyImg.src = toyObject.image
+        toyImg.className = "toy-avatar"
+
+        const toyP = document.createElement("p")
+        toyP.textContent = toyObject.likes + " likes"
+
+        const toyButton = document.createElement("button")
+        toyButton.textContent = "like"
+
+        toyCard.append(toyH2, toyImg, toyP, toyButton)
+
+        toyCardDiv.appendChild(toyCard)
+    
+    }
+
+
+
+
 });
